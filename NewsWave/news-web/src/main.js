9b44bd15e05726678c02dad05356b282f6287201
KaiyanWang <2022220360009@buu.edu.cn>
2025-06-09 16:50:30 +0800

    删除注释里的主机名

--- NewsWave/news-web/src/main.js
+++ NewsWave/news-web/src/main.js
@@ -7,7 +7,6 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 //配置根路径
-// const BASE_URL = 'http://39.99.43.230:8080';
 const BASE_URL = 'http://localhost:8080';
 //window里面的是全局内容,可以在任意script标签内部使用
 window.BASE_URL = BASE_URL;
